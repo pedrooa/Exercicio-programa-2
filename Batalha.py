6ea72c9dd10895693231a6028b03cc983fdeb210
Pedro Oliveira de Azambuja <pedroazambuja14@gmail.com>
2017-03-30 19:57:48 -0300

    Funcao Batalha versao 2

--- Batalha.py
+++ Batalha.py
@@ -1,18 +1,21 @@
-import random
-perdeu = False#Se perder a batalha, perdeu = True
-InsperDex=[]
-oponente = random.randrange(12)#Encontra um pokemon aleatorio
-def Batalha(meu,oponente):
-    print("Voce se deparou um um {0} selvagem!".format(Inspermons[oponente][nome]))
+
+def Batalha(meu,p,oponente):
+    import json
+    global InsperDex
+    InsperDex = []
+    with open('inspermons.json') as arquivo:
+        Inspermons = json.load(arquivo)
+    print("Voce se deparou um um {0} selvagem!".format(Inspermons[oponente]['nome']))
     if Inspermons[oponente] not in InsperDex:
         InsperDex.append(Inspermons[oponente])
-    while Inspermons[meu][vida] > 0 and Inspermons[oponente][vida]>0:
-        Inspermons[oponente][vida] = Inspermons[oponente][vida] - Inspermons[meu][ataque] + Inspermons[oponente][defesa]
-        if ipmon[oponente][vida] <= 0:
-            return("Você derrotou seu oponente!")
+    while meu['vida'] > 0 and Inspermons[oponente]['vida']>0:
+        Inspermons[oponente]['vida'] = Inspermons[oponente]['vida'] - meu['poder'] + Inspermons[oponente]['defesa']
+        if Inspermons[oponente]['vida'] <= 0:
+            print("Você derrotou seu oponente!")
+            print("A vida de seu Ipmon restante é: {0}".format(meu['vida']))
         else:
-            Inspermons[meu][vida] = Inspermons[meu][vida] - Inspermons[oponente][ataque] + Inspermons[meu][defesa]
-            if Inspermons[meu][vida] <= 0:
-                return("Você foi derrotado!")
+            meu['vida'] = meu['vida'] - Inspermons[oponente]['poder'] + meu['defesa']
+            if meu['vida'] <= 0:
+                print("Você foi derrotado!")
                 global perdeu
                 perdeu = True
--- ep02.py
+++ ep02.py
@@ -1,30 +1,53 @@
 #Ep02 - Inspermon
 import random
 import json
-
+from Batalha import Batalha
+InsperDex = []
 #Mostrar os tres pokemons iniciais em ordem numero de 0-2, para depois o usuario escolher qual deseja
 def inspermon_inicial(ipmon_inicial):
-    meu = ipmon_inicial
-    return meu
+    return Inspermons[ipmon_inicial]
 
-def mostra_inspermon(ipmon):
-    print('Inspermon : {0}'. format(ipmon['nome']))
-    print('Poder = {0}'. format(ipmon['poder']))
-    print('Defersa = {0}'. format(ipmon['defesa']))
-    print('Vida =  {0} \n'. format(ipmon['vida']))
+def mostra_inspermon(p):
+    print('Inspermon : {0}'. format(Inspermons[p]['nome']))
+    print('Poder = {0}'. format(Inspermons[p]['poder']))
+    print('Defersa = {0}'. format(Inspermons[p]['defesa']))
+    print('Vida =  {0} \n'. format(Inspermons[p]['vida']))
 
 
 with open('inspermons.json') as arquivo:
-    inspermons = json.load(arquivo)
+    Inspermons = json.load(arquivo)
 #for ipmon in inspermons:
     #mostra_inspermon(ipmon)
 
 while True:
-    açao_usuario = input('Você pode andar e procurar Inspermons ou dormir para recuperar sua vida. \n Digite 1 para andar ou 0 para dormir: ')
+    açao_usuario = int(input('Você pode andar e procurar Inspermons ou dormir para recuperar sua vida. \n Digite 1 para andar ou 0 para dormir: '))
     if açao_usuario == 0:
         print('Dormindo')
+        break#O jogo acaba quando o jogador vai dormir
     elif açao_usuario == 1:
         print('Andando')
-    print('Qual pokemon deseja usar: \n Charmander (0) \n Squirtle (1) \n Bulbasaur(2) \n')
-    print(inspermon_inicial(ipmon))
-    ipmon_inicial_var = input('Digite o numero do pokemon (0 - 2): ')
+        p = int(input('Qual pokemon deseja usar: \n Charmander (0) \n Squirtle (1) \n Bulbasaur(2) \n'))#obs: funciona com outros pokemons se o jogador escolher numeros acima de 2
+        meu = inspermon_inicial(p)
+        print("Stats iniciais de seu Ipmons: {0}".format(meu))
+        oponente = random.randrange(len(Inspermons))
+        perdeu = False
+        Batalha(meu,p, oponente)
+        if perdeu == True:
+            print('seu Inspermon esta inconsciente, va a helpdesk imediatamente recuperar a vida dele!')
+            break
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+

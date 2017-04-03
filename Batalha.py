18e28764bb72d3ad405f808dc815b38e68324108
Pedro Oliveira de Azambuja <pedroazambuja14@gmail.com>
2017-04-03 01:02:34 -0300

    Funcão fuga funcionando e outras modificacoes no codigo da batalha

--- Batalha.py
+++ Batalha.py
@@ -1,21 +1,34 @@
 from Insperdex import mostra_Insperdex as Insperdex
 import json
+import random
 
 def Batalha(meu,p,oponente,Inspermons):
     print("\nVoce se deparou com um {0} selvagem!".format(Inspermons[oponente]['nome']))
-    fuga = input('\nDeseja fugir da batalha? (1) Sim e (0) Não \n')
+    fuga = int(input('\nDeseja fugir da batalha? (1) Sim e (0) Não \n'))
     if fuga == 1:
-        #Falta InsperDex grava o pokemon
-        print('\nVocê fugiu da batalha!')
+        n = random.random()
+        if n<=0.51:
+            print('\nVocê fugiu da batalha!')
+        else:
+            print('\nFuga sem sucesso!')
+            while meu['vida'] > 0 and Inspermons[oponente]['vida']>0:
+                Inspermons[oponente]['vida'] = Inspermons[oponente]['vida'] - meu['poder'] + Inspermons[oponente]['defesa']
+                if Inspermons[oponente]['vida'] <= 0:
+                    print("\nVocê derrotou seu oponente!")
+                    print("A vida de seu Ipmon restante é: \n{0}".format(meu['vida']))
+                    meu['xp'] +=10
+                else:
+                    meu['vida'] = meu['vida'] - Inspermons[oponente]['poder'] + meu['defesa']
+                    if meu['vida'] <= 0:
+                        print("\nVocê foi derrotado!")
     elif fuga == 0:
         while meu['vida'] > 0 and Inspermons[oponente]['vida']>0:
             Inspermons[oponente]['vida'] = Inspermons[oponente]['vida'] - meu['poder'] + Inspermons[oponente]['defesa']
             if Inspermons[oponente]['vida'] <= 0:
                 print("\nVocê derrotou seu oponente!")
                 print("A vida de seu Ipmon restante é: \n{0}".format(meu['vida']))
+                meu["xp"] +=10
             else:
                 meu['vida'] = meu['vida'] - Inspermons[oponente]['poder'] + meu['defesa']
-                #O print funciona mas quando retorna Verdadeiro o if do ep02 não funciona
                 if meu['vida'] <= 0:
                     print("\nVocê foi derrotado!")
-                    return True #Não sei se há necessidade disso
--- ep02.py
+++ ep02.py
@@ -10,14 +10,12 @@ def inspermon_inicial(ipmon_inicial):
 def mostra_inspermon(p):
     print('\n Inspermon : {0}'. format(Inspermons[p]['nome']))
     print('Poder = {0}'. format(Inspermons[p]['poder']))
-    print('Defersa = {0}'. format(Inspermons[p]['defesa']))
+    print('Defesa = {0}'. format(Inspermons[p]['defesa']))
     print('Vida =  {0} \n'. format(Inspermons[p]['vida']))
 
 
 with open('inspermons.json') as arquivo:
     Inspermons = json.load(arquivo)
-#for ipmon in inspermons:
-    #mostra_inspermon(ipmon)
 
 Insperdex = []
 
@@ -28,17 +26,23 @@ while True:
         break #O jogo acaba quando o jogador vai dormir
     elif açao_usuario == 1:
         print('\n Andando...')
-        p = int(input('\nQual pokemon deseja usar: \nCharmander (0) \nSquirtle (1) \nBulbasaur(2)\n'))#obs: funciona com outros pokemons se o jogador escolher numeros acima de 2
+        p = int(input('\nQual InsperMon deseja usar: \nCharmander (0) \nSquirtle (1) \nBulbasaur(2)\n'))#obs: funciona com outros pokemons se o jogador escolher numeros acima de 2
         meu = inspermon_inicial(p)
         if meu['nome'] not in Insperdex:
             Insperdex.append(meu['nome'])
-        print("\nStats iniciais de seu Ipmons: \n{0}".format(meu))
+        print("\nStats iniciais de seu InsperMon: \n{0}".format(meu))
         oponente = random.randrange(len(Inspermons))
         if Inspermons[oponente]['nome'] not in Insperdex:
             Insperdex.append(Inspermons[oponente]['nome'])
         Batalha(meu,p, oponente,Inspermons)
+        hp = int(input("Bem Vindo ao helpdesk! Digite (1) para recuperar a vida de seu Inspermon, ou (0) para continuar: "))
+        if hp ==1:
+            meu["vida"] = Inspermons[p]["vida"]
+        if meu["xp"] >= 50:
+            print("Seu InsperMon está evoluindo!")
+            meu["xp"] = 0
+            meu["poder"] +=15
+            meu["vida"] += 100
+            meu["defesa"] +=5
         print('\nSeu InsperDex é este:\n{0}'. format(Insperdex))
-        #Não está funcionando este if
-        if Batalha == True:
-            print('\nSeu Inspermon esta inconsciente, va a helpdesk imediatamente recuperar a vida dele!\n')
-            break
+        

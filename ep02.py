9c4dbf6ad97c8fd4987b9bd197a43cc29bcd2e35
Pedro Oliveira de Azambuja <pedroazambuja14@gmail.com>
2017-04-04 15:17:40 -0300

    Adicionada a funcionalidade da xp e dicionario dos ipmon do jogador

--- ep02.py
+++ ep02.py
@@ -3,6 +3,32 @@ import random
 import json
 from Batalha import Batalha
 
+meus_ipmon= [
+        {
+        "nome" : "Charmander",
+        "poder" : 25,
+        "vida" : 200,
+        "defesa" : 10,
+        "xp" : 0
+    },
+    {
+        "nome" : "Squirtle",
+        "poder" : 25,
+        "vida" : 200,
+        "defesa" : 10,
+        "xp" : 0
+    },
+    {
+        "nome" : "Bulbasaur",
+        "poder" : 25,
+        "vida" : 200,
+        "defesa" : 10,
+        "xp" : 0
+    }]
+
+
+
+
 #Mostrar os tres pokemons iniciais em ordem numero de 0-2, para depois o usuario escolher qual deseja
 def inspermon_inicial(ipmon_inicial):
     return Inspermons[ipmon_inicial]
@@ -27,22 +53,30 @@ while True:
     elif açao_usuario == 1:
         print('\n Andando...')
         p = int(input('\nQual InsperMon deseja usar: \nCharmander (0) \nSquirtle (1) \nBulbasaur(2)\n'))#obs: funciona com outros pokemons se o jogador escolher numeros acima de 2
-        meu = inspermon_inicial(p)
-        if meu['nome'] not in Insperdex:
-            Insperdex.append(meu['nome'])
-        print("\nStats iniciais de seu InsperMon: \n{0}".format(meu))
+        #meus_ipmon[p] = inspermon_inicial(p)
+        if meus_ipmon[p]['nome'] not in Insperdex:
+            Insperdex.append(meus_ipmon[p]['nome'])
+        print("\nStats iniciais de seu InsperMon: \n{0}".format(meus_ipmon[p]))
         oponente = random.randrange(len(Inspermons))
         if Inspermons[oponente]['nome'] not in Insperdex:
             Insperdex.append(Inspermons[oponente]['nome'])
-        Batalha(meu,p, oponente,Inspermons)
-        hp = int(input("Bem Vindo ao helpdesk! Digite (1) para recuperar a vida de seu Inspermon, ou (0) para continuar: "))
-        if hp ==1:
-            meu["vida"] = Inspermons[p]["vida"]
-        if meu["xp"] >= 50:
+        Batalha(meus_ipmon[p],p, oponente,Inspermons)
+        while True:
+            hp = int(input("Bem Vindo ao helpdesk! Digite (1) para recuperar a vida de seu Inspermon, ou (0) para continuar: "))
+            if hp ==1 or hp ==0:
+                break
+            else:
+                print('Escreva direito!')
+        print(meus_ipmon[p]["vida"])
+        print(Inspermons[p]["vida"])
+        if hp == 1:
+            meus_ipmon[p]["vida"] = Inspermons[p]["vida"]
+            print(meus_ipmon[p])
+        if meus_ipmon[p]["xp"] >= 50:
             print("Seu InsperMon está evoluindo!")
-            meu["xp"] = 0
-            meu["poder"] +=15
-            meu["vida"] += 100
-            meu["defesa"] +=5
+            meus_ipmon[p]["xp"] = 0
+            meus_ipmon[p]["poder"] +=15
+            meus_ipmon[p]["vida"] += 100
+            meus_ipmon[p]["defesa"] +=5
         print('\nSeu InsperDex é este:\n{0}'. format(Insperdex))
         

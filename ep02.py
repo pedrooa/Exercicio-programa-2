47258031d5f118c4c06851c6cff1399afe295be5
pedrooa <pedroazambuja14@gmail.com>
2017-04-08 21:45:27 -0300

    Criada a lista vidas/mudança no codigo da evolução
    A lista vida armazena valores de referência para a vida, evitando bugs no jogo.
    Agora a evolução garante que a vida do ipmon vai ser igual ao valor referencia +100

--- ep02.py
+++ ep02.py
@@ -1,14 +1,73 @@
 #Ep02 - Inspermon
 import random, json
-from funções import função
+#from funções import função
 from colorama import Fore, Back, Style
 from Batalha import Batalha
+
+import sys, time
+
+meus_ipmon = list()
+
+class função:
+    def lista_meus_ipmons(Inspermons):
+        for i in range(3):
+            meus_ipmon.append(Inspermons[i])
+        return meus_ipmon
+
+    def escrita_timer(estringue):
+        for char in estringue:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.1)
+
+    #Funções não utilizadas
+    def mostra_inspermon(Inspermons,p):
+        print('\n Inspermon : {0}'. format(Inspermons[p]['nome']))
+        print('Poder = {0}'. format(Inspermons[p]['poder']))
+        print('Defesa = {0}'. format(Inspermons[p]['defesa']))
+        print('Vida =  {0}\n'. format(Inspermons[p]['vida']))
+
+    def inspermon_inicial(ipmon_inicial):
+        return Inspermons[ipmon_inicial]
+
+    #Corrige a vida negativa dos Inspermons
+    def correcao_vida(Inspermons, p):
+        if Inspermons[p]['vida'] < 0:
+            Inspermons[p]['vida'] = 0
+            return Inspermons[p]['vida']
+
+    def mostra_insperdex(Insperdex):
+        for e in Insperdex:
+            print(e)
+    #Determina qual ipmon inicial
+    def inspermon_usuario(Inspermons):
+        charm = Inspermons[0]['nome']
+        squir = Inspermons[1]['nome']
+        bulb = Inspermons[2]['nome']
+        vari = True
+        while vari:
+            p = int(input('\nQual InsperMon deseja usar: \n{0} (0) \n{1} (1) \n{2} (2)\n'.format(charm,squir,bulb))) #meus_ipmon[p] = inspermon_inicial(p)
+            if p == 0 or p == 1 or p == 2 and Inspermons[p]['vida'] != 0:
+                vari = False
+                return p
+            else:
+                if Inspermons[p]['vida'] == 0:
+                    print("\nEste Inspermon não está mais entre nós. Escolha outro\n")
+                else:
+                    print('\nDigite o numero referente ao pokemon que deseja, corretamente.')
+
+
+
 
 with open('inspermons.json') as arquivo:
     Inspermons = json.load(arquivo)
 
 Insperdex = []
+#Cria a lista com os ipmon iniciais e cria uma lista com os valores padrão de vida
 meus_ipmon = função.lista_meus_ipmons(Inspermons)
+vidas = []
+for e in meus_ipmon:
+    vidas.append(e["vida"])
 traço = "------"*15
 Checkpoint = [meus_ipmon]  #Prevenção de erros caso o jogador tente dar Save sem ter jogado nada.
 
@@ -19,6 +78,7 @@ while True:
             dados = json.load(arquivo)
         meus_ipmon = dados[0]
         Insperdex = dados[1]
+        vidas = dados[2]
         break
     elif load == 2:
         break
@@ -47,8 +107,16 @@ while True:
     elif açao_usuario == 1:
         #Pergunta ao usuário qual inspermon ele quer usar
         p = função.inspermon_usuario(meus_ipmon)
+        if meus_ipmon[p]['vida'] == 0:
+            print('\nSeu InsperMon possui zero de vida!')
+            string = "\nCurando ...\n"
+            função.escrita_timer(string)
+            print('InsperMon Curado!')
+            meus_ipmon[p]['vida'] = vida
+
+
         #Armazena a vida inicial do ipmon
-        vida = meus_ipmon[p]['vida']
+        vida = vidas[p]
         #Separa com traços
         print('\n' + traço + '\n')
         #Roda a função timer para a string
@@ -85,13 +153,14 @@ while True:
             print("\nSeu InsperMon está evoluindo!")
             meus_ipmon[p]["xp"] = 0
             meus_ipmon[p]["poder"] +=15
-            meus_ipmon[p]["vida"] += 100
+            vidas[p] += 100
+            meus_ipmon[p]["vida"] = vidas[p]
             meus_ipmon[p]["defesa"] +=5
         #Mostra o Insperdex
         print('\n' + traço + '\n')
         print('\nSeu Insperdex é:')
         função.mostra_insperdex(Insperdex)
-        #Salva meusipmon e insperdex
-        Checkpoint = [meus_ipmon,Insperdex]
+        #Salva meusipmon, insperdex e referencia dos valores de vida
+        Checkpoint = [meus_ipmon,Insperdex,vidas]
     else:
         print("\nCódigo incorreto, redigite!")

7d1e311143204fb6e2d560714f9ce405b7f613eb
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2017-04-10 10:48:14 -0300

    Timer nos rounds

--- Batalha.py
+++ Batalha.py
@@ -1,9 +1,11 @@
-import json, colorama, random
+import json, random, time
+from colorama import Fore, Back, Style
 
 traço = "------"*15
 
 def Batalha(meu,p,oponente,Inspermons):
     print("\nVoce se deparou com um {0} selvagem!".format(Inspermons[oponente]['nome']))
+    val = True
     while val:
         fuga = int(input('\nDeseja fugir da batalha? (1) Sim e (2) Não \n'))
         if fuga == 1:
@@ -29,6 +31,7 @@ def Batalha(meu,p,oponente,Inspermons):
             while meu['vida'] > 0 and Inspermons[oponente]['vida']>0:
                 print('\n' + traço + '\n')
                 print("\nRound {0} :" .format(e))
+                time.sleep(1)
                 print("\nO {0} possui {1} de vida  ---- O {2} possui {3} de vida" .format(meu['nome'], meu['vida'], Inspermons[oponente]['nome'], Inspermons[oponente]['vida']))
                 e += 1
                 Inspermons[oponente]['vida'] = Inspermons[oponente]['vida'] - meu['poder'] + Inspermons[oponente]['defesa']
